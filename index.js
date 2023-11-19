9c5bc70a506ab31d7ea2c3721179bc58bc7246f6
ydivya01@gmail.com <Ydivya01>
2023-11-19 15:55:20 +0530

    backend4

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors());
 app.use(cors({
-    origin:"http://localhost:3000",
+    // origin:"http://localhost:3000",
     //origin:["https://shimmering-kheer-34c615.netlify.app"],
+    origin:"https://keen-horse-3ef527.netlify.app/",
     methods:["GET", "POST", "PUT", "DELETE"],
     credentials:true,
     optionSuccessStatus:200
