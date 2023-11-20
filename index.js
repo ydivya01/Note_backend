364a8a71ef40edd0be0d861e3e51184a29506c82
ydivya01@gmail.com <Ydivya01>
2023-11-21 02:52:27 +0530

    backend11

--- index.js
+++ index.js
@@ -24,14 +24,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// app.use(cors({
-//     // origin:"http://localhost:3000",
-//     //origin:["https://shimmering-kheer-34c615.netlify.app"],
-//     origin:"https://keen-horse-3ef527.netlify.app",
-//     methods:["GET", "POST", "PUT", "DELETE"],
-//     credentials:true,
-//     optionSuccessStatus:200
-// }))
+app.use(cors({
+    // origin:"http://localhost:3000",
+    origin:"https://keen-horse-3ef527.netlify.app"
+ 
+}))
 
 
 //Get all notes
